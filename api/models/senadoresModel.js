2d1bda519be13d90982a0f6a8f572f9f42263b03
Carlos <carlos.h.silva.vieira@gmail.com>
2017-11-18 11:46:59 -0200

    incluindo bibliotecas

--- api/models/senadoresModel.js
+++ api/models/senadoresModel.js
@@ -2,28 +2,28 @@ var connection = require('./connection');
 
 module.exports.getSenadores = function()
 {
-	var query = "select * from deputados";
+	var query = "select * from senadores";
 	var resultado = connection.query(query);
 	return resultado;
 }
 
 module.exports.getSenador = function(nome)
 {
-	var query = "select * from deputados where nome = '"+ nome + "'";
+	var query = "select * from senadores where nome = '"+ nome + "'";
 	var resultado = connection.query(query);
 	return resultado;
 }
 
 module.exports.getTweetsSenadores = function()
 {
-	var query = "select * from tweets_deputados";
+	var query = "select * from tweets_senadores";
 	var resultado = connection.query(query);
 	return resultado;
 }
 
 module.exports.getTweetsSenador = function(id_parlamentar)
 {
-	var query = "select * from tweets_deputados where id_parlamentar = "+ id_parlamentar + "";
+	var query = "select * from tweets_senadores where id_parlamentar = "+ id_parlamentar + "";
 	var resultado = connection.query(query);
 	return resultado;
 }
